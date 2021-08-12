5fd01ef6764877ef12474f57dbe5cfe86588eab7
RahiniManrai <73948618+RahiniManrai@users.noreply.github.com>
2021-08-12 08:25:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "INVERTEBRATES";
+const accessCode2 = "VENUS";
+const accessCode3 = "ANKARA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E R T B I R T V A S E E N", 100,50)
+    fill("blue")
+    text("Hint: Creatures who don't have a spine!!", 100,80)
+
+    text("S V E N U", 700,150)
+    fill("lightblue")
+    text("Hint: Hottest planet", 700,180)
+
+    text("K R A N A A", 300,250)
+    fill("blue")
+    text("Hint: Capital of Turkey!", 300,270)
+    
+
+}
